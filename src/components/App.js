fa1147b8f46373e2a414023285b390bfa0f1ce96
Your NameLakasi <you@example.com>
2022-03-02 04:12:02 -0300

    desafio2-AlmadaLautaro

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import NavBar from "./NavBar"
+import "./estilos.css"
+
+function App () {
+    return <NavBar/>
+};
+export default App
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar(){
+    return (
+        <nav>
+            <h1>Tienda de videojuegos</h1>
+            <p> <a href="">Ofertas - </a>
+                <a href="">Todos los juegos - </a>
+                <a href="">Contacto</a>
+            </p>
+        </nav>
+    )
+    
+}
+
+export default NavBar
